31733e37210a9b0639e648e6d115adc65a5160f3
jonathams <jonatha.schmitz@gmail.com>
2018-06-12 22:15:58 -0300

    updated put method

--- my-spark-app/src/main/java/app/Main.java
+++ my-spark-app/src/main/java/app/Main.java
@@ -6,11 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import app.entity.Book;
 import static app.utils.JsonUtil.*;
-
 
 public class Main {
 
@@ -19,13 +20,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Main service");
 		post("/books", (request, response) -> {
-						 
+
 			ObjectMapper mapper = new ObjectMapper();
 			Book book = new Book();
 			book = mapper.readValue(request.body(), Book.class);
-		    System.out.println(book.getAuthor() + ' ' + book.getTitle() + ' ' + book.getIsbn());
 
 			books.put(String.valueOf(book.getIsbn()), book);
 
@@ -34,9 +33,9 @@ public class Main {
 
 		});
 
-		// Gets the book resource for the provided id
-		get("/books/:id", (request, response) -> {
-			Book book = books.get(request.params(":id"));
+		// Gets the book resource for the provided isbn
+		get("/books/:isbn", (request, response) -> {
+			Book book = books.get(request.params(":isbn"));
 			if (book != null) {
 				return "Title: " + book.getTitle() + ", Author: " + book.getAuthor();
 			} else {
@@ -47,47 +46,47 @@ public class Main {
 
 		// updates the book for the provided id with the author and title send through
 		// the query param
-		put("/books/:id", (request, response) -> {
-			String id = request.params(":id");
-			Book book = books.get(id);
+		put("/books/:isbn", (request, response) -> {
+			String isbn = request.params(":isbn");
+			Book book = books.get(isbn);
 			if (book != null) {
-				String newAuthor = request.queryParams("author");
-				String newTitle = request.queryParams("title");
+				String newAuthor = readProperty("author", request.body());
+				String newTitle = readProperty("title", request.body());
 				if (newAuthor != null) {
 					book.setAuthor(newAuthor);
 				}
 				if (newTitle != null) {
 					book.setTitle(newTitle);
 				}
-				return "Book with id '" + id + "' updated";
+				return "Book with isbn '" + isbn + "' updated";
 			} else {
 				response.status(404); // 404 Not found
 				return "Book not found";
 			}
 		});
 
-		// delete a book from the list based on the id received from the request
-		delete("/books/:id", (request, response) -> {
-			String id = request.params(":id");
+		// delete a book from the list based on the isbn received from the request
+		delete("/books/:isbn", (request, response) -> {
+			String isbn = request.params(":isbn");
 			;
-			Book book = books.get(id);
+			Book book = books.get(isbn);
 			if (book != null) {
 				books.remove(book);
-				return "Book with id " + id + " removed!";
+				return "Book with isbn " + isbn + " removed!";
 			} else {
 				response.status(404);
 				return "Book not found";
 			}
 		});
 
-		 get("/books", (request, response) ->{
-			 if(books != null && books.size() > 0 ) {
-				 response.type("application/json");
-				 return dataToJson(books);
-			 } else {
-				 return "Sorry, No book was found";
-			 }
-		 });
+		get("/books", (request, response) -> {
+			if (books != null && books.size() > 0) {
+				response.type("application/json");
+				return dataToJson(books);
+			} else {
+				return "Sorry, No book was found";
+			}
+		});
 
 		// port(8080);
 		// simple get
--- my-spark-app/src/main/java/app/utils/JsonUtil.java
+++ my-spark-app/src/main/java/app/utils/JsonUtil.java
@@ -1,6 +1,7 @@
 package app.utils;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 
@@ -9,15 +10,32 @@ import spark.utils.ObjectUtils;
 import java.io.*;
 
 public class JsonUtil {
-    public static String dataToJson(Object data) {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.enable(SerializationFeature.INDENT_OUTPUT);
-            StringWriter sw = new StringWriter();
-            mapper.writeValue(sw, data);
-            return sw.toString();
-        } catch (IOException e) {
-            throw new RuntimeException("IOEXception while mapping object (" + data + ") to JSON");
-        }
-    }
+	public static String dataToJson(Object data) {
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.enable(SerializationFeature.INDENT_OUTPUT);
+			StringWriter sw = new StringWriter();
+			mapper.writeValue(sw, data);
+			return sw.toString();
+		} catch (IOException e) {
+			throw new RuntimeException("IOEXception while mapping object (" + data + ") to JSON");
+		}
+	}
+
+	public static String readProperty(String property, String json) {
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			JsonNode rootNode = mapper.readTree(json);
+			JsonNode propertyNode = rootNode.path(property);
+			return propertyNode.textValue();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return "";
+	}
 }
